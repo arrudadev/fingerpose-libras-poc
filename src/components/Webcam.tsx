0668424dd075a328599743423650ca9c2f62d849
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-11-16 23:44:42 -0300

    add handpose-detection model and tensorflow js

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -1,10 +1,54 @@
+import 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-core@4.2.0/dist/tf-core.min.js'
+import 'https://unpkg.com/@tensorflow/tfjs-backend-webgl@3.7.0/dist/tf-backend-webgl.min.js'
+import 'https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.4.1646424915/hands.min.js'
+import 'https://cdn.jsdelivr.net/npm/@tensorflow-models/hand-pose-detection@2.0.0/dist/hand-pose-detection.min.js'
+import 'https://cdn.jsdelivr.net/npm/fingerpose@0.1.0/dist/fingerpose.min.js'
+
 import { useEffect, useRef } from 'react'
+
+declare global {
+  interface Window {
+    VERSION: string
+    handPoseDetection: any
+  }
+}
 
 export function Webcam() {
   const videoRef = useRef<HTMLVideoElement>(null)
+  let detector: any
 
   function browserSupportMediaDevices() {
     return navigator?.mediaDevices || navigator?.mediaDevices.getUserMedia
+  }
+
+  async function loadModel() {
+    if (!detector) {
+      const handsVersion = window.VERSION
+      const detectorConfig = {
+        runtime: 'mediapipe', // or 'tfjs',
+        solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${handsVersion}`,
+        // full é o mais pesado e o mais preciso
+        modelType: 'lite',
+        maxHands: 2,
+      }
+
+      detector = await window.handPoseDetection.createDetector(
+        window.handPoseDetection.SupportedModels.MediaPipeHands,
+        detectorConfig,
+      )
+    }
+  }
+
+  async function estimateHands(camera: HTMLVideoElement) {
+    const predictions = await detector.estimateHands(camera, {
+      flipHorizontal: true,
+    })
+
+    if (predictions?.length) console.log(predictions)
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    // requestAnimationFrame(estimateHands(camera))
   }
 
   async function setupCamera(camera: HTMLVideoElement) {
@@ -31,7 +75,7 @@ export function Webcam() {
     camera.play()
   }
 
-  useEffect(() => {
+  async function init() {
     if (!browserSupportMediaDevices()) {
       throw new Error(
         `Browser API navigator.mediaDevices.getUserMedia not available`,
@@ -39,8 +83,15 @@ export function Webcam() {
     }
 
     const camera = videoRef.current as HTMLVideoElement
-    setupCamera(camera)
-  }, [videoRef])
+
+    await setupCamera(camera)
+    await loadModel()
+    await estimateHands(camera)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <div className="h-full w-full shadow-xl p-4 rounded">
