fc4398651956d624ea7d1aa609bb29c764ff5809
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-11-18 14:24:48 -0300

    add F letter sign

--- src/signs/index.ts
+++ src/signs/index.ts
@@ -3,5 +3,6 @@ import { BSign } from './BSign'
 import { CSign } from './CSign'
 import { DSign } from './DSign'
 import { ESign } from './ESign'
+import { FSign } from './FSign'
 
-export const knownGestures = [ASign, BSign, CSign, DSign, ESign]
+export const knownGestures = [ASign, BSign, CSign, DSign, ESign, FSign]
